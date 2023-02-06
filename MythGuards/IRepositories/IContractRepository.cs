b9bc5f9f58ff3d83d0c2046f588a58a9d6b550ec
Morganero <morganero1@gmail.com>
2023-02-06 10:03:57 -0600

    did work all the way up to repositories

---
+++ MythGuards/IRepositories/IContractRepository.cs
@@ -0,0 +1,13 @@
+﻿using MythGuards.Models;
+using System.Collections.Generic;
+
+namespace MythGuards.IRepositories
+{
+    public interface IContractRepository
+    {
+        void Add(Contract contract);
+        void Edit(Contract contract);
+        List<Contract> GetAllContracts();
+        Contract GetById(int id);
+    }
+}
---
+++ MythGuards/IRepositories/IUserProfileRepository.cs
@@ -0,0 +1,15 @@
+﻿using MythGuards.Models;
+using System.Collections.Generic;
+
+namespace MythGuards.IRepositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        void Edit(UserProfile userProfile);
+        List<UserProfile> GetAllUserProfiles();
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        UserProfile GetById(int id);
+        void SoftDelete(UserProfile userProfile);
+    }
+}
