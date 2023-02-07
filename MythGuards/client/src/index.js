044d55208a62437fc0458c8acac80a0b02d5cb8c
Morganero <morganero1@gmail.com>
2023-02-07 10:58:43 -0600

    finished controllers and tested Get funcionality

---
+++ MythGuards/client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+};
+firebase.initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+reportWebVitals();
