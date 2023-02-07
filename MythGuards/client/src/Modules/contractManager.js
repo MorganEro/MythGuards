e8bb74951e4a90fc3916337a5d465c76c9f394b5
Morganero <morganero1@gmail.com>
2023-02-07 11:45:46 -0600

    created 3 managers

--- MythGuards/client/src/Modules/contractManager.js
+++ MythGuards/client/src/Modules/contractManager.js
@@ -1,25 +1,35 @@
 const apiUrl = '/api/Contract/';
 
-export const getInstruments = () => {
+export const GetContracts = () => {
   return fetch(apiUrl)
     .then(resp => resp.json());
 };
 
-export const searchInstruments = (criterion) => {
-  return fetch(`${apiUrl}search?q=${criterion}`)
-    .then(resp => resp.json());
-};
-
-export const getInstrument = (id) => {
+export const GetContract= (id) => {
   return fetch(apiUrl + id).then(resp => resp.json());
 };
 
-export const updateInstrument = (instrument) => {
-  return fetch(apiUrl + instrument.id, {
+export const UpdateContract = (contract) => {
+  return fetch(apiUrl + contract.id, {
     method: 'PUT',
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(instrument)
+    body: JSON.stringify(contract)
+  });
+};
+export const AddContract = (contract) => {
+  return fetch(apiUrl + contract.id, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(contract)
+  });
+};
+
+export const DeleteContract = (contract) => {
+  return fetch(apiUrl + contract.id, {
+    method: 'DELETE',
   });
 };
